4f264f789b3f1b4aba2905180d7f0e8dc876f8e3
nolan <nolan796@live.ca>
2019-12-09 20:06:00 -0700

    Create query to fetch, total rev all, avg rev all, # stores over M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total= Store.sum(:annual_revenue)
+@avg = Store.average(:annual_revenue)
+
+@sev_figures = Store.where("annual_revenue > 1000000").count
+
+puts "This is the total annual revenue of all stores : #{@total}"
+puts "This is the average revenue of all stores      : #{@avg}"
+puts "This is the # of stores over $1M in revenue    : #{@sev_figures}"
